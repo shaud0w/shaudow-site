026aeeae1845439f193a73097fdf9c4474913936
shaud0w <shaudowrk@gmail.com>
2022-03-08 15:48:49 +0800

    header styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import PFP from '../../assets/pfp.JPG';
+import PFP from '../../assets/pfp2.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
@@ -11,16 +11,18 @@ const Header = () => {
         <h5>Hi I&apos;m</h5>
         <h1>Shaudow</h1>
         <h5 className='text-light'>I trade and build...</h5>
-        <CTA></CTA>
-      </div>
-      <HeaderSocials />
-      <div className='me'>
-        <img src={PFP} alt='Shaudow PFP' />
-      </div>
 
-      <a href='#contact' className='scroll__down'>
-        Reach out
-      </a>
+        <CTA></CTA>
+
+        <HeaderSocials />
+
+        <a href='#contact' className='scroll__down'>
+          Reach out
+        </a>
+        <div className='me'>
+          <img src={PFP} alt='Shaudow PFP' />
+        </div>
+      </div>
     </header>
   );
 };
